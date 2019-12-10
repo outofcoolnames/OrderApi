185587e47b982e6490242ff9928b055e2bcb30c4
Thomas Cournane <thomas.cournane@amcsgroup.com>
2019-12-10 12:03:58 +0000

    updated auth

--- OrderApi.Utils/RequestUtils.cs
+++ OrderApi.Utils/RequestUtils.cs
@@ -14,9 +14,16 @@ namespace OrderApi.Utils
         /// <returns>THe username supplied</returns>
         public string GetClient(HttpRequest request)
         {
-            var authHeader = AuthenticationHeaderValue.Parse(request.Headers["Authorization"]).Parameter;
-            string decodedToken = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader));
-            return decodedToken.Substring(0, decodedToken.IndexOf(":"));
+            var authHeaderDictionary = request.Headers["Authorization"];
+            
+            if (authHeaderDictionary.Count >0)
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(authHeaderDictionary).Parameter;
+                string decodedToken = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader));
+                return decodedToken.Substring(0, decodedToken.IndexOf(":"));
+            }
+            return string.Empty;
+            
         }
     }
 }
